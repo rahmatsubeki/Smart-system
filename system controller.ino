751d61f7b28c21ab16667cdb081564d275ed0f0d
beky <91580555+rahmatsubeki@users.noreply.github.com>
2023-10-18 09:33:44 +0700

    Update system controller.ino

--- system controller.ino
+++ system controller.ino
@@ -1,89 +1,114 @@
 #define BLYNK_PRINT Serial
-
-#define BLYNK_TEMPLATE_ID "TMPL6jFzsNsnD"
-#define BLYNK_TEMPLATE_NAME "monitor"
-
-#ifndef STASSID
-#define STASSID "RMT"
-#define STAPSK "dawarblandong"
-#endif
-
-#include <ESP8266WiFi.h>
-#include <BlynkSimpleEsp8266.h>
-
-#include <ESP8266mDNS.h>
-#include <WiFiUdp.h>
-#include <ArduinoOTA.h>
-//serial jaringan
-#include <TelnetStream.h>
-
-
+#define BLYNK_TEMPLATE_ID "TMPL6L8dR8HBQ"
+#define BLYNK_TEMPLATE_NAME "System Plts"
+#define BLYNK_FIRMWARE_VERSION "1.0"
 #include <Wire.h>
 #include <Adafruit_INA219.h>
+#include <ESP8266WiFi.h>
+#include <ESP8266HTTPClient.h>
+#include <ESP8266httpUpdate.h>
+#include <BlynkSimpleEsp8266.h>
+#include <NTPClient.h>
+#include <WiFiUdp.h>
+#include <ArduinoOTA.h>
+WiFiClient client;
 
+#define UPDATE_BUTTON_PIN V9
+
+// WiFi credentials dan token Blynk
+char auth[] = "Zu7zjfvslnsdPjd0F4HMfIG5CL00_vOw";
+const char* ssid = "RMT";
+const char* password = "dawarblandong";
+const char* firmwareURL = "https://github.com/rahmatsubeki/Smart-system/blob/main/system%20controller.ino";  // Ganti URL dengan URL firmware di GitHub
+const int currentFirmwareVersion = 1; 
+
+// Sensor INA219
 Adafruit_INA219 ina219;
-unsigned long previousMillis = 0;
-unsigned long interval = 2000;
 
+// Pin Relay
+const int relayPin1 = D5;
+const int relayPin2 = D6;
+const int relayPin3 = D7;
+const int relayPin4 = D8;
 
-const int analogpin= A0;
-const int relayPin = D8;
+// NTP Client
+WiFiUDP ntpUDP;
+NTPClient timeClient(ntpUDP, "pool.ntp.org");
+
+//progamina
 const float noVoltageThreshold = 0.1;
-
-//blynk
-char auth[] = "a4yGOxotTKYYGbUgrB2vivNfuHGBs3rS";
-const char* ssid = STASSID;
-const char* password = STAPSK;
-
 float shuntvoltage = 0.00;
 float busvoltage = 0.00;
-float current = 0.00;
 float loadvoltage = 0.00;
-float energy = 0.00,  energyCost, energyPrevious, energyDifference;
-float energyPrice = 6.50 ;
+float current = 0.00;
 float power = 0.00;
+float energy = 0.00,  energyCost, energyPrevious, energyDifference;
 float capacity = 0.00;
+float avgBusVoltage = 0.0;
+const int numReadings = 10;
+float calculateAverageBusVoltage() {
+  float totalBusVoltage = 0.0;
+
+  for (int i = 0; i < numReadings; i++) {
+    // Membaca nilai tegangan bus
+    float busVoltage = ina219.getBusVoltage_V();
+    totalBusVoltage += busVoltage;
+
+    // Menunggu sebelum membaca nilai berikutnya
+    delay(1000); // Tunggu 1 detik (1000 ms)
+  }
+
+  // Menghitung rata-rata tegangan bus selama periode waktu
+  float averageBusVoltage = totalBusVoltage / numReadings;
+  return averageBusVoltage;
+}
+
+BLYNK_WRITE(V9) {  // Fungsi ini akan dipanggil saat tombol di Blynk ditekan
+  int buttonState = param.asInt();
+  if (buttonState == HIGH) {  // Tombol ditekan
+    Blynk.virtualWrite(V10,"Memeriksa Pembaruan...");
+  }
+}
 
 void setup() {
   Serial.begin(115200);
-  while(!Serial){
-    delay(1);
-  }
-  
-  uint32_t currentFrequency;
-  if(!ina219.begin()){
-    Serial.println("Sensor Tidak Ditemukan");
-    while(1){
-      delay(10);
-    }
-  }
-  // put your setup code here, to run once:
-  pinMode(relayPin, OUTPUT);
   Blynk.begin(auth, ssid, password);
-  WiFi.mode(WIFI_STA);
-  WiFi.begin(ssid, password);
-  while (WiFi.waitForConnectResult() != WL_CONNECTED) {
-    Serial.println("Connection Failed! Rebooting...");
-    delay(5000);
-    ESP.restart();
-  }
-  ArduinoOTA.onStart([]() {
-    String type;
-    if (ArduinoOTA.getCommand() == U_FLASH) {
-      type = "sketch";
-    } else {  // U_FS
-      type = "filesystem";
-    }
+  ina219.begin();
+  pinMode(relayPin1, OUTPUT);
+  pinMode(relayPin2, OUTPUT);
+  pinMode(relayPin3, OUTPUT);
+  pinMode(relayPin4, OUTPUT);
 
-    // NOTE: if updating FS this would be the place to unmount FS using FS.end()
+  // Synchronize waktu dengan server NTP
+  configTime(7 * 3600, 0, "pool.ntp.org");
+
+  // Tunggu sampai terhubung ke WiFi
+  while (WiFi.status() != WL_CONNECTED) {
+    delay(1000);
+    Serial.println("Connecting to WiFi...");
+  }
+  Serial.println("Connected to WiFi");
+
+  // Set waktu yang benar setelah mendapatkan koneksi WiFi
+  timeClient.begin();
+  while (!timeClient.update()) {
+    timeClient.forceUpdate();
+  }
+  Serial.println("Time synchronized");
+
+  ArduinoOTA.onStart([]() {
+    String type = (ArduinoOTA.getCommand() == U_FLASH) ? "sketch" : "filesystem";
     Serial.println("Start updating " + type);
   });
+
   ArduinoOTA.onEnd([]() {
     Serial.println("\nEnd");
   });
+
   ArduinoOTA.onProgress([](unsigned int progress, unsigned int total) {
     Serial.printf("Progress: %u%%\r", (progress / (total / 100)));
   });
+
   ArduinoOTA.onError([](ota_error_t error) {
     Serial.printf("Error[%u]: ", error);
     if (error == OTA_AUTH_ERROR) {
@@ -98,59 +123,27 @@ void setup() {
       Serial.println("End Failed");
     }
   });
-  //OTA
+
+  ArduinoOTA.setHostname("Batrei_Monitoring");
   ArduinoOTA.begin();
-  TelnetStream.begin();
 }
 
 void loop() {
-  ArduinoOTA.handle();
-
   Blynk.run();
-  unsigned long currentMillis = millis();
-  if (currentMillis - previousMillis >= interval)
-  {
-    previousMillis = currentMillis;
-    sistemku();
-  } 
-}
-void sistemku(){
-//sell surya
-  float R1 = 30000.0;
-  float R2 = 7500.0;
-  float hasil= 0.0;
-  int analogValue = analogRead(analogpin);
-  float value = analogValue * (3.3/1023.0);
-  hasil = (value / (R2 / (R1+R2)));
-  if (value < noVoltageThreshold) {
-    hasil = 0.0;
-  }
-
-  //sytem
+  timeClient.update();
+  ArduinoOTA.handle();
+  // Dapatkan waktu saat ini
+  int currentHour = (timeClient.getHours() + 7)%24;
+  int currentMinute = timeClient.getMinutes();
+  
+  // Mengaktifkan relay pada pukul 17:30 dan mematikannya pada pukul 05:30
+  loadvoltage = busvoltage + (shuntvoltage / 1000);
+  power = loadvoltage * current;
   shuntvoltage = ina219.getShuntVoltage_mV();
   busvoltage = ina219.getBusVoltage_V();
   current = ina219.getCurrent_mA();
-  loadvoltage = busvoltage + (shuntvoltage / 1000);
-  power = loadvoltage * current;
-  energy = energy + power / 3600; //Wh
-  capacity = capacity + current / 1000;
-  energyDifference = energy - energyPrevious;
-  energyPrevious = energy;
-  energyCost = energyCost + (energyPrice / 1000 * energyDifference);
-  if (loadvoltage < 11) {
-    // Jika nilai arus kurang dari 1 A
-    digitalWrite(relayPin, LOW);  // Menghidupkan relay
-    Blynk.virtualWrite(V5, String("System OFF"));
-    Blynk.virtualWrite(V6, 1);
-    Serial.println("Nilai arus kurang dari 1 A - Relay ON");
-  } else {
-    // Jika nilai arus tidak kurang dari 1 A
-    digitalWrite(relayPin, HIGH);  // Mematikan relay
-    Blynk.virtualWrite(V5, String("System ON"));
-    Blynk.virtualWrite(V6, 0);
-    Serial.println("Nilai arus lebih dari atau sama dengan 1 A - Relay OFF");
-  }
-  TelnetStream.println(busvoltage);
+  energy = energy + power / 3600 / 1000; //kwh
+  //deklarasi berdasarkan
   if (loadvoltage < 1 )loadvoltage = 0;
   if (current < 1 )
   {
@@ -160,30 +153,61 @@ void sistemku(){
     capacity = 0;
     energyCost=0;
   }
-  Serial.print("Voltage:   "); Serial.print(value); Serial.println(" V");
-  Serial.print("Voltage pengukuran:   "); Serial.print(hasil); Serial.println(" V");
-  Serial.print("hasil bacaan:   "); Serial.println(analogValue); 
-  Serial.print("Bus Voltage:   "); Serial.print(busvoltage); Serial.println(" V");
-  Serial.print("Shunt Voltage: "); Serial.print(shuntvoltage); Serial.println(" mV");
-  Serial.print("Load Voltage:  "); Serial.print(loadvoltage); Serial.println(" V");
-  Serial.print("Current:       "); Serial.print(current); Serial.println(" mA");
-  Serial.print("Power:         "); Serial.print(power); Serial.println(" mW");
-  Serial.print("Energy:        "); Serial.print(energy); Serial.println(" Wh");
-  Serial.println("-------------------------");
 
-  // put your main code here, to run repeatedly:
-  Blynk.virtualWrite(V0, hasil);//plts
-  Blynk.virtualWrite(V1, (current / 1000) );//amper pengguna
-  Blynk.virtualWrite(V2, (power / 1000) );//watt pengguna
-  Blynk.virtualWrite(V3, (energy / 1000));// konsumsi
-  Blynk.virtualWrite(V7, loadvoltage);//voltase pengguna
-  // CAPACITY
-  if (capacity > 1000) {
-    Blynk.virtualWrite(V4, String((capacity / 1000), 2) + String(" Ah") );
+  if(loadvoltage > 11.0){
+      digitalWrite(relayPin1, HIGH);
+      // digitalWrite(relayPin2, HIGH);
+
+      if ((currentHour == 4 && currentMinute >= 15) || (currentHour >= 4 && currentHour <= 16) || (currentHour == 16 && currentMinute <= 30)) {
+        avgBusVoltage = calculateAverageBusVoltage();
+        digitalWrite(relayPin2, LOW);
+      } else {
+        avgBusVoltage = 0.0;
+        digitalWrite(relayPin2, HIGH);
+      }
+  }else{
+      digitalWrite(relayPin1, LOW);
+      digitalWrite(relayPin2, LOW);
   }
-  else
-  {
-    Blynk.virtualWrite(V4, String((capacity), 2) + String(" mAh") );
+
+
+  // Kirim data ke Blynk
+  if (Blynk.connected()) {
+    Blynk.virtualWrite(V0, avgBusVoltage);
+    Blynk.virtualWrite(V1, loadvoltage);//voltase pengguna
+    Blynk.virtualWrite(V2, (current / 1000));//amper pengguna
+    Blynk.virtualWrite(V3, (power / 1000));//watt pengguna
+    Blynk.virtualWrite(V4, energy);// konsumsi
   }
-  
+
+  // Tunggu 1 detik sebelum membaca sensor dan mengirim data lagi
+  delay(5000);
 }
+
+void checkUpdateButton() {
+  int updateButtonState = digitalRead(UPDATE_BUTTON_PIN);
+
+  // Jika tombol update ditekan
+  if (updateButtonState == LOW) {
+    Serial.println("Tombol update ditekan!");
+    
+    // Mengupdate kode dari URL firmware yang ditentukan
+    t_httpUpdate_return ret = ESPhttpUpdate.update(client, "https://github.com/rahmatsubeki/Smart-system/blob/main/system%20controller.ino");
+
+    // Memeriksa hasil pembaruan
+    switch (ret) {
+      case HTTP_UPDATE_FAILED:
+        Serial.printf("Update gagal, error (%d): %s\n", ESPhttpUpdate.getLastError(), ESPhttpUpdate.getLastErrorString().c_str());
+        break;
+        
+      case HTTP_UPDATE_NO_UPDATES:
+        Serial.println("Tidak ada pembaruan tersedia.");
+        break;
+        
+      case HTTP_UPDATE_OK:
+        Serial.println("Pembaruan berhasil!");
+        break;
+    }
+  }
+}
+
